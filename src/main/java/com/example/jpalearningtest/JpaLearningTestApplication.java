f7c7d7437b9468d1d1e40f2c76e5b902eee1eda0
Daeun Lee <leede0418@likelion.org>
2021-08-14 23:52:05 +0900

    chore: 프로젝트 기본 세팅

---
+++ src/main/java/com/example/jpalearningtest/JpaLearningTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpalearningtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLearningTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLearningTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpalearningtest/JpaLearningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpalearningtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLearningTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
