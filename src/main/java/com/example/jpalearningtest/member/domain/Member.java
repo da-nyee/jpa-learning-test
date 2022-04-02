6ab8363dc71d398b7b1f463e38747d5a264217cd
Daeun Lee <leedaeun.dev@gmail.com>
2022-04-03 00:21:49 +0900

    feat: Member 엔티티 기본 세팅

---
+++ src/main/java/com/example/jpalearningtest/member/domain/Member.java
@@ -0,0 +1,23 @@
+package com.example.jpalearningtest.member.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Member() {
+    }
+
+    public Member(String name) {
+        this.name = name;
+    }
+}
